428eec9dafb2cb95388c507d9d24ef2aaca98b53
varunakoju301 <varunakoju301@gmail.com>
2023-10-18 00:34:58 -0400

    First commit

---
+++ src/App.js
@@ -0,0 +1,55 @@
+// App.js
+import React, { useState } from 'react';
+
+ const App = () => {
+//   const [loginUsername, setLoginUsername] = useState('');
+//   const [loginPassword, setLoginPassword] = useState('');
+//   const [registerUsername, setRegisterUsername] = useState('');
+//   const [registerPassword, setRegisterPassword] = useState('');
+
+//   const handleLogin = () => {
+//     // Add your login logic here
+//     console.log('Login:', loginUsername, loginPassword);
+//   };
+
+//   const handleRegister = () => {
+//     // Add your registration logic here
+//     console.log('Register:', registerUsername, registerPassword);
+//   };
+
+//   return (
+//     <div>
+//       <h1>Login</h1>
+//       <input
+//         type="text"
+//         placeholder="Username"
+//         value={loginUsername}
+//         onChange={(e) => setLoginUsername(e.target.value)}
+//       />
+//       <input
+//         type="password"
+//         placeholder="Password"
+//         value={loginPassword}
+//         onChange={(e) => setLoginPassword(e.target.value)}
+//       />
+//       <button onClick={handleLogin}>Login</button>
+
+//       <h1>Register</h1>
+//       <input
+//         type="text"
+//         placeholder="Username"
+//         value={registerUsername}
+//         onChange={(e) => setRegisterUsername(e.target.value)}
+//       />
+//       <input
+//         type="password"
+//         placeholder="Password"
+//         value={registerPassword}
+//         onChange={(e) => setRegisterPassword(e.target.value)}
+//       />
+//       <button onClick={handleRegister}>Register</button>
+//     </div>
+//   );
+};
+
+export default App;
